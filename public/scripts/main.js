9f442a148f687153306a2c85c0c1897d4ae83efa
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-06-25 20:57:00 -0300

    Adicionando funcionalidae de adicionar e remover o modal

---
+++ public/scripts/main.js
@@ -0,0 +1,14 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+//Pega todos os botões de classe check
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button =>{
+    button.addEventListener("click", event =>{
+        //Abre o modal chamando o arquivo modal.js
+        modal.open() 
+    })
+})
+
