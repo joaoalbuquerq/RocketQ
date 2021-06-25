2952d99757602ccb27e277e41b3b82c295e24967
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-06-25 19:52:13 -0300

    Modificando estrutura do projeto para se adaptar as rotas

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ const server = express() //Atribui a server todo o conteúdo de express
 
 server.set('view engine', 'ejs') //seja no express que a view engine é o ejs
 
+server.use(express.static("public")) //Indicando que arquivos estáticos como estilos e imagens ficaram na pasta public
+
 //faz uma juntção do caminho do projeto com o dirname(que é src por causa do join)
 server.set('views', path.join(__dirname, 'views'))
 
