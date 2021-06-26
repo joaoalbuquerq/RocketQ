3bbac18181f9c9024c8104fe9b7a7e117095a4b3
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-06-26 12:49:32 -0300

    Adicionando funcionalidade de abrir e fechar o modal de acordo com o botão clicado

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,13 +2,39 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
-//Pega todos os botões de classe check
+//Mapeia os elementos do modal
 const checkButtons = document.querySelectorAll(".actions a.check")
+const modalTitle = document.querySelector(".modal h2")
+const modalDescription = document.querySelector(".modal p")
+const modalButton = document.querySelector(".modal button")
+
 
 checkButtons.forEach(button =>{
-    button.addEventListener("click", event =>{
-        //Abre o modal chamando o arquivo modal.js
-        modal.open() 
-    })
+    button.addEventListener("click", handleClick)
 })
 
+
+/*Procura no html a classe actions que dentro dela possui tags A com a classe delete
+a cada delete encontrado irá abrir o modal */
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+
+// Função que troca o conteúdo do modal de acordo com o botão clicado, e por final abre o modal
+function handleClick(event,check=true){
+
+    event.preventDefault()
+    const text = check ? 'Marcar como lida' : 'Excluir '
+
+    modalTitle.innerHTML = `${text} esta pergunta `
+    modalDescription.innerHTML = `Deseja realmente ${text.toLowerCase()} esta pergunta ?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+
+    modal.open()
+}
