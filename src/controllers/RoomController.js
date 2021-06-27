3a93305ba925574f2f58413f4bb1b38d7e499b67
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-06-27 10:30:38 -0300

    Adicionando validação de número da sala

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -4,26 +4,37 @@ module.exports = {
     async create(req,res){
         const db = await Database()
         const pass = req.body.password
-        let roomId
+        let roomId 
+        let isRoom = true
 
-        for(var i=0; i<6 ; i++){
+        while(isRoom){
 
-            i==0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-            roomId += Math.floor(Math.random() * 10).toString()
+            // GERA NÚMERO DA SALA
+            for(var i=0; i<6 ; i++){
+
+                i==0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+                roomId += Math.floor(Math.random() * 10).toString()
+                }
             
+            //VERIFICA SE O NÚMERO DA SALA JÁ EXISTE
+             const roomExistIds = await db.all(`SELECT id FROM rooms`)
+             isRoom = roomExistIds.some(roomExistId => roomExistId === roomId)
 
+             if(!isRoom){
+
+                // INSERÇÃO DE DADOS NO BANCO
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES(
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+    
+            }
         }
 
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES(
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
-
-        db.close()
-
+        await db.close()
         res.redirect(`/room/${roomId}`)
     }
 }
