6f31110008100cac9ecae726906bf75cbd066ab2
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-06-27 10:43:09 -0300

    Criando maneira de no html pegar o id de sala referente a que se encontra

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -36,5 +36,10 @@ module.exports = {
 
         await db.close()
         res.redirect(`/room/${roomId}`)
+    },
+
+    open(req,res){
+        const roomId = req.params.room
+        res.render("room", {roomId: roomId})
     }
 }
