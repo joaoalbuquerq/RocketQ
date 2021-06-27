3f595fec882ec75f619b0f5a1aeff5df44d15eea
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-06-26 21:44:23 -0300

    Adicionando a geração dinamica de ids da sala

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,15 @@
 module.exports = {
     create(req,res){
-        let roomId = 101456
+        let roomId
+
+        for(var i=0; i<6 ; i++){
+
+            i==0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+            roomId += Math.floor(Math.random() * 10).toString()
+            
+
+        }
+
 
         res.redirect(`/room/${roomId}`)
     }
