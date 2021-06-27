a49b5493c750af46b6290b8b5b1931e6584ca06d
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-06-27 13:38:05 -0300

    Adicionando configurações adicionais

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
-const express = require('express') //Faz uma requisição ao express
-const route = require('./route') //Importa o arquivo de rotas
-const path = require('path') //módulo para indicar caminho da view
+const express = require('express')
+const route = require('./route')
+const path = require('path')
 
+const server = express()
 
-const server = express() //Atribui a server todo o conteúdo de express
+server.set('view engine', 'ejs')
 
-server.set('view engine', 'ejs') //seja no express que a view engine é o ejs
+server.use(express.static("public"))
 
-server.use(express.static("public")) //Indicando que arquivos estáticos como estilos e imagens ficaram na pasta public
-
-//faz uma juntção do caminho do projeto com o dirname(que é src por causa do join)
 server.set('views', path.join(__dirname, 'views'))
 
-server.use(express.urlencoded({extended: true})) //Middleware
+server.use(express.urlencoded({extended: true}))
 
-server.use(route) //Setando que o express irá usar o arquivo de rotas
+server.use(route)
 
-server.listen(3000, () => console.log('rodando'))
+server.listen(3000, () => console.log("RODANDO"))
