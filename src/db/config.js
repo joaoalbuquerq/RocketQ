022840b48203abf65ac9aa638d72d9bdadacb754
jotapedr0 <joaopealbuquerque1@gmail.com>
2021-06-27 13:36:15 -0300

    Modificando controladores para se comportarem dinamicamente

--- src/db/config.js
+++ src/db/config.js
@@ -1,5 +1,5 @@
-const sqlite3 = require("sqlite3")
-const { open } = require("sqlite") //Trás apenas a função open do sqlite
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite")
 
 module.exports = () => 
     open({
